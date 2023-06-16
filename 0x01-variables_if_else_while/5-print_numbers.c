6751072ae2d3d65dc6053efb768fd30d35a4dd63
Brokeice123 <keithmarkomen@gmail.com>
2023-06-16 16:22:22 +0000

    a program that prints all single digit numbers of base 10 starting from 0, followed by a new line.

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - Checks whether the last digit of a number is positive or negatie
+ *
+ * Return: Always 0. Otherwise 1
+ */
+
+int main(void)
+{
+	int num = 0;
+
+	while (num <= 10)
+	{
+		if (num != 10)
+		printf("%d", num);
+		num++;
+	}
+	putchar('\n');
+
+	return (0);
+}

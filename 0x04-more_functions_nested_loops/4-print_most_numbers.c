2ddfa870d042f893e881903dac7e5cbe6c6058a4
Brokeice123 <keithmarkomen@gmail.com>
2023-06-22 12:58:15 +0000

    a function that prints 0 - 9 followed by a new line, except 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -11,11 +11,9 @@ void print_most_numbers(void)
 
 	for (n = '0'; n <= '9'; n++)
 	{
-		while (n != '2' || n != '4')
-		{
-			_putchar(n);
-		}
-
+		if (n == 50 || n == 52)
+			continue;
+		_putchar(n);
 	}
 	_putchar(10);
 }

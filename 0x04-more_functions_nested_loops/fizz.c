e809675d742f84d5ed57f12e63cbb590a4a355e8
Brokeice123 <keithmarkomen@gmail.com>
2023-06-22 18:48:11 +0000

    FizzBuzz Program

---
+++ 0x04-more_functions_nested_loops/fizz.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+/**
+ *main - test to print the fizz buzz test
+ *Return: 0 is successfull
+ **/
+
+int main(void)
+{
+	int n;
+
+	for (n = 1; n <= 100; n++)
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", n);
+		}
+	}
+
+	return (0);
+}

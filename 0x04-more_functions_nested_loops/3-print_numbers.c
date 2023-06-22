afcd75c86578ce0be3eb2c1bbbaa21e8e029079e
Brokeice123 <keithmarkomen@gmail.com>
2023-06-22 10:49:06 +0000

    a function that prints 0 - 9 followed by a new line

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_numbers - Prints numbers from 0- 9
+ *
+ **/
+
+void print_numbers(void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+	{
+		_putchar(n);
+	}
+	_putchar(10);
+}

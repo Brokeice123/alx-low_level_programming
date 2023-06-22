3a22d58af78c351faf839fb13679a28ca2f0a1c9
Brokeice123 <keithmarkomen@gmail.com>
2023-06-22 11:41:33 +0000

    a function that prints 0 - 9 followed by a new line

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,10 +8,12 @@
 void print_numbers(void)
 {
 	int n;
+	char num_char;
 
 	for (n = 0; n < 10; n++)
 	{
-		_putchar(n);
+		num_char = '0' + n;
+		_putchar(num_char);
+		_putchar(10);
 	}
-	_putchar(10);
 }

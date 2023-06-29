c2b6c1d8e3cb515589d5cdf7a09e596faf2bb403
Brokeice123 <keithmarkomen@gmail.com>
2023-06-29 21:19:01 +0000

    a function that capitalizes all words of a string.

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *cap_string - Capitalizes all words of a string
+ *@str: String to be Capitalized
+ *Return: str
+ */
+
+char *cap_string(char *str)
+{
+	int i = 0, flag = 1;
+
+	for (; *(s + i) != '\0'; i++)
+	{
+		if (
+				s[i] == 32 || s[i] == 9 || s[i] == 10 ||
+				s[i] == 44 || s[i] == 59 || s[i] == 46 ||
+				s[i] == 33 || s[i] == 63 || s[i] == '"' ||
+				s[i] == 40 || s[i] == 41 ||
+				s[i] == 123 || s[i] == 125
+			)
+
+			flag = 1;
+		else if (flag)
+		{
+			if (s[i] >= 'a' && s[i] <= 'z')
+				s[i] = s[i] - 'a' + 'A';
+
+			flag = 0;
+		}
+	}
+
+	return (s);
+}

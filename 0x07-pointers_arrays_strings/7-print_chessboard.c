317b73a6fade7acb59618d8c90cba53a18bb8c5a
Brokeice123 <keithmarkomen@gmail.com>
2023-07-04 00:30:10 +0300

    prints the chessboard.

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print_chessboard - Prints the chessboard
+ *@a: The chessboard
+ */
+
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; a[i][7]; i++)
+	{
+		for (j = 0; j < 8; j++)
+			_putchar(a[i][j]);
+
+		_putchar(10);
+	}
+
+}

4c1bc8ed0de7e2d9bbce0208e0194c00c543038e
Brokeice123 <keithmarkomen@gmail.com>
2023-07-10 16:06:09 +0300

    a function that returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,6 +1,8 @@
 #ifndef MALLOC_FREE_H
 #define MALLOC_FREE_H
 
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);

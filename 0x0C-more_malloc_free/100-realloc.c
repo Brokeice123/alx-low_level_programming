b1fb3149d9fabc9cdee371cc8fa5aadbdd6ef27f
Brokeice123 <keithmarkomen@gmail.com>
2023-07-14 00:48:48 +0300

    a function that reallocates a memory block using malloc and free

---
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_memcpy - Copies memory Area
+ *@dest: Destination
+ *@src: Source
+ *@n: Number of Bytes
+ *Return: dets(Destination)
+ */
+
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+
+	return (dest);
+}
+
+/**
+ *_realloc - reallocates a memory block using malloc and free
+ *@ptr: a pointer to the memory previously allocated
+ *@old_size: The old size
+ *@new_size: The new size
+ *Return: A pointer to the new mem
+ */
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	void *new_ptr;
+
+	if (ptr == NULL)
+		return (malloc(new_size));
+
+	if (new_size == 0)
+	{
+		free(ptr);
+		return (NULL);
+	}
+
+	new_ptr = malloc(new_size);
+
+	if (new_ptr == NULL)
+		return (NULL);
+
+	_memcpy(new_ptr, ptr, old_size);
+	free(ptr);
+
+	return (new_ptr);
+
+}

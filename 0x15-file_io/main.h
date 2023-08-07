ea2dd4ed684720efbd8330cb24bd72246856f6ae
Brokeice123 <keithmarkomen@gmail.com>
2023-08-07 15:09:45 +0300

    a function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -2,6 +2,8 @@
 #define FILE_IO_H
 
 #include <stdlib.h>
+#include <unistd.h>
+#include <fcntl.h>
 
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
